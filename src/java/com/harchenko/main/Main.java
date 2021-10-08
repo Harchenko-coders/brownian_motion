fe51b912bf8e52188e76a4170f7fd8964830af51
lugovskyi <lvcom64@gmail.com>
2021-10-08 14:04:11 +0300

    edited Main class

--- src/java/com/harchenko/main/Main.java
+++ src/java/com/harchenko/main/Main.java
@@ -1,7 +1,37 @@
 package java.com.harchenko.main;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of cells");
+        int number_of_cells = scanner.nextInt();
+
+        System.out.println("Enter number of admixture");
+        int number_of_admixture = scanner.nextInt();
+
+        int[] cells = new int[number_of_cells];
+
+        cells[0] = number_of_admixture;
+
+        for(int i = 0; i < number_of_cells; i++){
+
+        MyTread myTread = new MyTread();
+        myTread.start();
+
+        }
+    }
+}
+class MyTread extends Thread {
+    public void run() {
+
+    }
+}
+
+class Runner implements Runnable {
+    @Override
+    public void run(){
 
     }
 }
